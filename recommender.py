1805a9c0dea1e6e7b4f8f90905f89b96b7358c2d
Felsal <felipessalvador@googlemail.com>
2017-01-10 11:04:17 -0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -9,13 +9,52 @@ from datetime import datetime, timedelta
 import dfFunctions
 import tf_models
 
+class SVDmodel(object):
+    
+    def __init__(self,dataframe,users, items, ratings):
+        self.dataframe = dataframe
+        self.users = users
+        self.items = items
+        self.ratings = ratings
+        self.size = len(dataframe)
+        self.num_of_users = max(self.dataframe[self.users]) + 1
+        self.num_of_items = max(self.dataframe[self.items]) + 1
+        self.train,self.test,self.valid = self.data_separation()
+        
+    def data_separation(self):
+        rows = len(self.dataframe)
+        df = self.dataframe.iloc[np.random.permutation(rows)].reset_index(drop=True)
+        split_index = int(rows * 0.9)
+        new_split = split_index + int((rows - split_index) *0.5)
+        df_train = df[0:split_index]
+        df_test = df[split_index: new_split].reset_index(drop=True)
+        df_validation = df[new_split:].reset_index(drop=True)
+        return df_train, df_test,df_validation
+
+
+    def training(self,hp_dim,hp_reg,learning_rate,batch_size,num_steps):
+        self.train_batches = dfFunctions.BatchGenerator(self.train,batch_size,self.users,self.items,self.ratings)
+        self.test_batches = dfFunctions.BatchGenerator(self.test,batch_size,self.users,self.items,self.ratings)
+        self.valid_batches = dfFunctions.BatchGenerator(self.valid,len(self.valid),self.users,self.items,self.ratings)
+        self.tf_model = tf_SVD(self.num_of_users,self.num_of_items,self.train_batches,self.test_batches,self.valid_batches)
+        self.tf_model.training(hp_dim,hp_reg,learning_rate,batch_size,num_steps)
+        self.tf_model.print_stats()
+
+    def valid_prediction(self):
+        self.tf_model.prediction(show_valid=True)
+       
+    def prediction(self,list_of_users,list_of_items):
+        return self.tf_model.prediction(list_of_users,list_of_items)
+
 
 class tf_SVD(object):
 
-    def __init__(self,num_of_users,num_of_items,batch_generator):
+    def __init__(self,num_of_users,num_of_items,train_batch_generator,test_batch_generator,valid_batch_generator):
         self.num_of_users = num_of_users
         self.num_of_items = num_of_items
-        self.batch_generator = batch_generator
+        self.train_batch_generator = train_batch_generator
+        self.test_batch_generator = test_batch_generator
+        self.valid_batch_generator = valid_batch_generator
         self.general_duration = 0 
         self.num_steps = 0
         self.dimension = None
@@ -70,7 +109,6 @@ class tf_SVD(object):
     def training(self,hp_dim,hp_reg,learning_rate,batch_size,num_steps):
         self.get_graph(hp_dim,hp_reg,learning_rate)
         self.num_steps = num_steps
-        self.batch_size = batch_size
         marker = ''
 
         with tf.Session(graph=self.graph) as sess:
@@ -79,11 +117,11 @@ class tf_SVD(object):
             start = time.time()
             initial_time = start
             for step in range(num_steps):
-                users, items, rates = self.batch_generator('train')
+                users, items, rates = self.train_batch_generator.get_batch()
                 feed_dict = {self.tf_user_batch: users, self.tf_item_batch: items, self.tf_rate_batch: rates}         
                 _, pred_batch,cost,train_error = sess.run([self.train_op, self.infer, self.tf_cost,self.acc_op], feed_dict=feed_dict)
                 if (step % 1000)  == 0:
-                    users, items, rates = self.batch_generator('test') 
+                    users, items, rates = self.test_batch_generator.get_batch() 
                     feed_dict = {self.tf_user_batch: users, self.tf_item_batch: items, self.tf_rate_batch: rates}              
                     pred_batch = sess.run(self.infer, feed_dict=feed_dict)
                     test_error = self.accuracy(pred_batch,rates)
@@ -96,8 +134,7 @@ class tf_SVD(object):
                     print("{:3d} {:f} {:f}{:s} {:f} {:f}(s)".format(step, train_error, test_error,marker,cost,
                                                            end - start))
                     marker = ''
-                    start = end
-        #duration of the whole process  
+                    start = end 
         self.general_duration = time.time() - initial_time
 
     def print_stats(self):
@@ -116,7 +153,7 @@ class tf_SVD(object):
             self.get_graph(self.dimension,self.regularizer,self.learning_rate)
             with tf.Session(graph=self.graph) as sess:
                 self.saver.restore(sess=sess, save_path=self.save_path)
-                users, items, rates = self.batch_generator('valid')
+                users, items, rates = self.valid_batch_generator.get_batch()
                 if show_valid:
                     feed_dict = {self.tf_user_batch: users, self.tf_item_batch: items, self.tf_rate_batch: rates}
                     valid_error = sess.run(self.acc_op, feed_dict=feed_dict)
@@ -126,93 +163,6 @@ class tf_SVD(object):
                     prediction = sess.run(self.infer, feed_dict=feed_dict)
                     return prediction    
 
-
-
-class SVDmodel(object):
-    
-    def __init__(self,dataframe,users, items, ratings):
-        self.dataframe = dataframe
-        self.users = users
-        self.items = items
-        self.ratings = ratings
-        self.array_users = np.array(dataframe[self.users])
-        self.array_items = np.array(dataframe[self.items])
-        self.array_ratings = np.array(dataframe[self.ratings])
-        self.num_cols = len(dataframe.columns)
-        self.size = len(dataframe)
-        self.general_duration = 0 
-        self.num_steps = 0
-        self.dimension = None
-        self.regularizer = None
-        #assuming that the dataframe has n items started with 0
-        self.num_of_users = max(self.dataframe[self.users]) + 1
-        self.num_of_items = max(self.dataframe[self.items]) + 1
-        self.best_acc_test = float('inf')
-        self.train,self.test,self.valid = self.data_separation()
-        self.train_array_users = np.array(self.train[self.users])
-        self.train_array_items = np.array(self.train[self.items])
-        self.train_array_ratings = np.array(self.train[self.ratings])
-        self.train_size = len(self.train)
-        self.test_array_users = np.array(self.test[self.users])
-        self.test_array_items = np.array(self.test[self.items])
-        self.test_array_ratings = np.array(self.test[self.ratings])
-        self.test_size = len(self.test)
-        self.valid_array_users = np.array(self.valid[self.users])
-        self.valid_array_items = np.array(self.valid[self.items])
-        self.valid_array_ratings = np.array(self.valid[self.ratings])
-        self.valid_size = len(self.valid)
-
-        
-    def data_separation(self):
-        rows = len(self.dataframe)
-        df = self.dataframe.iloc[np.random.permutation(rows)].reset_index(drop=True)
-        split_index = int(rows * 0.9)
-        new_split = split_index + int((rows - split_index) *0.5)
-        df_train = df[0:split_index]
-        df_test = df[split_index: new_split].reset_index(drop=True)
-        df_validation = df[new_split:].reset_index(drop=True)
-        return df_train, df_test,df_validation
-
-
-    def get_batch(self,dataframe_name):
-        if dataframe_name == 'train':
-            size = self.train_size
-            array_users = self.train_array_users
-            array_items = self.train_array_items
-            array_ratings = self.train_array_ratings 
-        elif dataframe_name == 'test':  
-            size = self.test_size
-            array_users = self.test_array_users
-            array_items = self.test_array_items 
-            array_ratings = self.test_array_ratings 
-        elif dataframe_name == 'valid':  
-            size = self.valid_size 
-            array_users = self.valid_array_users
-            array_items = self.valid_array_items
-            array_ratings = self.valid_array_ratings 
-        else: 
-            print("ERROR")
-        random_indices = np.random.randint(0,size,self.batch_size)
-        users = array_users[random_indices]
-        items = array_items[random_indices]
-        ratings = array_ratings[random_indices]         
-        return users, items, ratings
-
-
-    def training2(self,hp_dim,hp_reg,learning_rate,batch_size,num_steps,stats=True):
-        self.batch_size = batch_size
-        self.tf_model = tf_SVD(self.num_of_users,self.num_of_items,self.get_batch)
-        self.tf_model.training(hp_dim,hp_reg,learning_rate,batch_size,num_steps)
-        if stats == True:
-            self.tf_model.print_stats()
-
-    def valid_prediction(self):
-        self.batch_size = self.valid_size
-        self.tf_model.prediction(show_valid=True)
-       
-    #def prediction2(self):
-    #    self.tf_model.prediction(show_valid=True)
-   
 
 
 if __name__ == '__main__':
@@ -226,9 +176,9 @@ if __name__ == '__main__':
     batch_size = 1000
     num_steps = 4000
 
-    model.training2(dimension,regularizer_constant,learning_rate,batch_size,num_steps)
+    model.training(dimension,regularizer_constant,learning_rate,batch_size,num_steps)
     model.valid_prediction()
-    #print(model.prediction(model.num_of_users,model.num_of_items,model.get_batch,np.array([0,0,0]),np.array([1192,660,913])))
+    print(model.prediction(np.array([0,0,0]),np.array([1192,660,913])))
 
 
         
